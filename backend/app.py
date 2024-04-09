61276bccaa3f0eb185918c0e78fae603d2979899
ruslan2020202 <ruslan020507@gmail.com>
2024-04-09 12:59:02 +0300

    Importerror the to a circular import

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,18 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
-from backend.database.db import db
-from backend.resources.routers import Todo
-from backend.config import Config
+from backend.database.models import db
+from backend.resources.routers import TodoTask
 
 
-def create_app():
+def create_app(config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config)
     api = Api(app)
     CORS(app)
-    import backend.database.models
     db.init_app(app)
     with app.app_context():
         db.create_all()
     Migrate(app, db)
-    api.add_resource(Todo, '/todo/api/v1', strict_slashes=False)
+    api.add_resource(TodoTask, '/todo/api/v1', strict_slashes=False)
     return app
