45b64b88c09bfe81095d1cfa1b3911c9d199a4aa
ruslan2020202 <ruslan020507@gmail.com>
2024-04-15 23:27:36 +0300

    add tests

--- backend/tests/inserts.py
+++ backend/tests/inserts.py
@@ -1,13 +1,17 @@
 from backend.database.models import db, TodoList
 from backend.wsgi import app
 
-test_data = ['come home', 'cook food', 'to do work']
 
-with app.app_context():
-    try:
-        for i in test_data:
-            db.session.add(TodoList(i))
-        db.session.commit()
-        print('Successfully inserted test data')
-    except Exception as e:
-        print(e)
+def insert_test_data():
+    test_data = ['come home', 'cook food', 'to do work']
+
+    with app.app_context():
+        try:
+            for i in test_data:
+                db.session.add(TodoList(i))
+            db.session.commit()
+            print('Successfully inserted test data')
+        except Exception as e:
+            print(e)
+
+
---
+++ backend/tests/test_api.py
@@ -0,0 +1,53 @@
+import pytest
+import backend.config as config
+from backend.app import create_app
+from backend.database.models import *
+from backend.tests.inserts import insert_test_data
+
+
+def find_id(app):
+    with app.app_context():
+        id_task = TodoList.query.first().id
+        return id_task
+
+
+@pytest.fixture
+def app():
+    app = create_app(config.TestingConfig)
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        insert_test_data()
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    client = app.test_client()
+    return client
+
+
+def test_get_tasks(client):
+    response = client.get('/todo/api/v1/tasks')
+    assert response.status_code == 200
+
+
+def test_get_task_by_id(client, app):
+    res = client.get(f'/todo/api/v1/tasks/{find_id(app)}')
+    assert res.status_code == 200
+
+
+def test_add_task(client):
+    data = {'name': 'Test task'}
+    res = client.post('/todo/api/v1/tasks', json=data)
+    assert res.status_code == 201
+
+
+def test_update_task(client, app):
+    res = client.patch(f'/todo/api/v1/tasks/{find_id(app)}')
+    assert res.status_code == 200
+
+
+def test_delete_task(client, app):
+    res = client.delete(f'/todo/api/v1/tasks/{find_id(app)}')
+    assert res.status_code == 200
