5a2b3c81a8ae29fc96b64221b89098770077407c
ruslan2020202 <ruslan020507@gmail.com>
2024-04-19 21:25:03 +0300

    changed config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.environ.get('DATABASE_USER')}:{os.environ.get('DATABASE_PASSWORD')}@db:3306/ToDo"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     FLASK_APP = os.environ.get('FLASK_APP')
